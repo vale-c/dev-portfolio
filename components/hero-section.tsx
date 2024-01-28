6687acf59e0a12663eabdd64e30766aa56ff8572
Valentina Calabrese <valent95@gmail.com>
2024-01-28 15:59:12 +0100

    added eslint ignore to escape doubleslashes

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
 "use client";
 import { SnakeGame } from "@/components/snake-game/snake-game";
 
--- components/snake-game/snake-game.tsx
+++ components/snake-game/snake-game.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
 import React, { useState, useEffect, useCallback } from "react";
 
 type Coordinate = {
